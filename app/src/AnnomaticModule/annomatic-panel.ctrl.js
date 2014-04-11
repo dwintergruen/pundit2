8d7428dadfd79bbf6e5f11bf94ff22d35a6c40ee
Simone Fonda <fonda@netseven.it>
2014-04-11 13:02:59 +0200

    Selecting stuff to try out the bookmarklet, step 1: getting the ranges

--- app/src/AnnomaticModule/annomatic-panel.ctrl.js
+++ app/src/AnnomaticModule/annomatic-panel.ctrl.js
@@ -1,23 +1,76 @@
 angular.module('Pundit2.AnnomaticModule')
-.controller('AnnomaticPanelCtrl', function($scope, Annotate) {
+.controller('AnnomaticPanelCtrl', function($scope, Annotate, $window) {
 
     // Get ready to use the service
     var gotAnnotations = false;
     $scope.getAnnotations = function() {
         if (gotAnnotations) return;
         gotAnnotations = true;
-        Annotate.getDataTXTAnnotations(angular.element('div.panel-body'));
+        Annotate.getDataTXTAnnotations(annotationsRootNode);
     };
     $scope.startReview = function() {
         Annotate.reviewNext(0);
     };
-    
     
     $scope.Annotate = Annotate;
     
     $scope.$watch('filteredTypes', function(filtered, oldFiltered) {
         if (typeof(filtered) === "undefined" && typeof(oldFiltered) === "undefined") return;
         Annotate.setTypeFilter(filtered);
+    });
+
+
+    var annotationsRootNode = angular.element('div.panel-body');
+
+    var getSelectedRange = function() {
+        var range;
+        
+        if ($window.getSelection().rangeCount === 0) {
+            // console.log('Range count 0!');
+            return null;
+        }
+
+        range = $window.getSelection().getRangeAt(0);
+
+        // If the selected range is empty (this happens when the user clicks on something)...
+        if  (range !== null && (range.startContainer === range.endContainer) && (range.startOffset === range.endOffset)) {
+            // console.log("Range is not null, but start/end containers and offsets match: no selected range.");
+            return null;
+        }
+
+        return range;
+
+    }; // getSelectedRange()
+    
+
+
+    var ancestor;
+    angular.element('body').on('mousedown', function(event) {
+
+        angular.element('body').on('mousemove', function(event) {
+            var r = getSelectedRange();
+            if (r && r.commonAncestorContainer !== ancestor) {
+                if (ancestor) {
+                    angular.element(ancestor).removeClass('selecting-ancestor');
+                }
+                ancestor = r.commonAncestorContainer;
+                
+                if (ancestor.nodeType === Node.TEXT_NODE) {
+                    ancestor = ancestor.parentNode;
+                }
+                angular.element(ancestor).addClass('selecting-ancestor');
+            }
+        });
+    });
+
+    angular.element('body').on('mouseup', function(event) {
+        angular.element('body').off('mousemove');
+        
+        if (ancestor) {
+            angular.element(ancestor).removeClass('selecting-ancestor');
+            annotationsRootNode = angular.element(ancestor);
+            $scope.getAnnotations();
+        }
     });
 
 });
