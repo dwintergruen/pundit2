06038a6b695a187e567263d1957d8bbf21d97051
SimoneSabba <sabbatini.simone@gmail.com>
2014-04-22 18:03:09 +0200

    - add toolbar directive - add toolbar template - add toolbar example

---
+++ app/src/Toolbar/Toolbar.js
@@ -0,0 +1,7 @@
+/**
+ * @ngdoc module
+ * @name Pundit2.Toolbar
+ * @module Pundit2.Toolbar
+ * @description Pundit 2Toolbar
+**/
+angular.module('Pundit2.Toolbar', []);
--- app/src/templates.js
+++ app/src/templates.js
@@ -1,4 +1,4 @@
-angular.module('templates-main', ['src/Annomatic/AnnomaticPanel.dir.tmpl.html', 'src/Annomatic/AnnotationPopover.tmpl.html', 'src/Dashboard/Dashboard.dir.tmpl.html']);
+angular.module('templates-main', ['src/Annomatic/AnnomaticPanel.dir.tmpl.html', 'src/Annomatic/AnnotationPopover.tmpl.html', 'src/Dashboard/Dashboard.dir.tmpl.html', 'src/Toolbar/Toolbar.dir.tmpl.html']);
 
 angular.module("src/Annomatic/AnnomaticPanel.dir.tmpl.html", []).run(["$templateCache", function($templateCache) {
   $templateCache.put("src/Annomatic/AnnomaticPanel.dir.tmpl.html",
@@ -57,4 +57,80 @@ angular.module("src/Annomatic/AnnotationPopover.tmpl.html", []).run(["$templateC
 angular.module("src/Dashboard/Dashboard.dir.tmpl.html", []).run(["$templateCache", function($templateCache) {
   $templateCache.put("src/Dashboard/Dashboard.dir.tmpl.html",
     "Dashboard Template!!!");
+}]);
+
+angular.module("src/Toolbar/Toolbar.dir.tmpl.html", []).run(["$templateCache", function($templateCache) {
+  $templateCache.put("src/Toolbar/Toolbar.dir.tmpl.html",
+    "<nav class=\"navbar navbar-inverse pnd-toolbar-navbar\" role=\"navigation\">\n" +
+    "    <div class=\"container-fluid pnd-toolbar-navbar-container\">\n" +
+    "    \n" +
+    "        <div class=\"collapse navbar-collapse\">\n" +
+    "            <ul class=\"nav navbar-nav\">\n" +
+    "                <li class=\"dropdown\">\n" +
+    "                    <a href=\"#\" class=\"dropdown-toggle\" data-toggle=\"dropdown\">\n" +
+    "                        <span class=\"glyphicon glyphicon-exclamation-sign\"></span>\n" +
+    "                    </a>\n" +
+    "                    <ul class=\"dropdown-menu\">\n" +
+    "                        <li>Please sign in to use Pundit</li>\n" +
+    "                        <li><a href=\"#\">Sign in</a></li>\n" +
+    "                    </ul>\n" +
+    "                </li> <!-- end dropdown-->\n" +
+    "                <li class=\"dropdown\">\n" +
+    "                    <a href=\"#\" class=\"dropdown-toggle\" data-toggle=\"dropdown\">\n" +
+    "                        <span class=\"glyphicon glyphicon-ok\"></span>\n" +
+    "                    </a>\n" +
+    "                    <ul class=\"dropdown-menu\">\n" +
+    "                        <li>You are signed in as MyName</li>\n" +
+    "                        <li><a href=\"#\">Sign Out</a></li>\n" +
+    "                    </ul>\n" +
+    "                </li> <!-- end dropdown-->\n" +
+    "                <li class=\"dropdown\">\n" +
+    "                    <a href=\"#\" class=\"dropdown-toggle\" data-toggle=\"dropdown\">\n" +
+    "                        <span class=\"glyphicon glyphicon-cog\"></span>\n" +
+    "                    </a>\n" +
+    "                    <ul class=\"dropdown-menu\">\n" +
+    "                        <li><a href=\"#\">My Config Menu 1</a></li>\n" +
+    "                        <li><a href=\"#\">My Config Menu 2</a></li>\n" +
+    "                        <li><a href=\"#\">My Config Menu 3</a></li>\n" +
+    "                    </ul>\n" +
+    "                </li> <!-- end dropdown-->\n" +
+    "\n" +
+    "                <li><a href=\"#\">Ask the Pundit</a></li>\n" +
+    "                <li><a href=\"#\">Dashboard</a></li>\n" +
+    "\n" +
+    "            </ul> <!-- nav navbar-nav -->\n" +
+    "\n" +
+    "            <ul class=\"nav navbar-nav navbar-right\">\n" +
+    "\n" +
+    "                <li><a href=\"#\"><span class=\"glyphicon glyphicon-pencil\"></span></a></li>\n" +
+    "\n" +
+    "                <li class=\"dropdown\">\n" +
+    "                    <a href=\"#\" class=\"dropdown-toggle\" data-toggle=\"dropdown\">Current template</a>\n" +
+    "                    <ul class=\"dropdown-menu\">\n" +
+    "                        <li><a href=\"#\">My Template 1</a></li>\n" +
+    "                        <li><a href=\"#\">My Template 2</a></li>\n" +
+    "                        <li><a href=\"#\">My Template 3</a></li>\n" +
+    "                    </ul>\n" +
+    "                </li> <!-- end dropdown-->\n" +
+    "\n" +
+    "                <li class=\"dropdown\">\n" +
+    "                    <a href=\"#\" class=\"dropdown-toggle\" data-toggle=\"dropdown\">My Notebooks</a>\n" +
+    "                    <ul class=\"dropdown-menu\">\n" +
+    "                        <li><a href=\"#\">Current Notebooks 1</a></li>\n" +
+    "                        <li><a href=\"#\">My Notebooks 1</a></li>\n" +
+    "                        <li><a href=\"#\">My Notebooks 2</a></li>\n" +
+    "                        <li><a href=\"#\">My Notebooks 3</a></li>\n" +
+    "                    </ul>\n" +
+    "                </li> <!-- end dropdown-->\n" +
+    "\n" +
+    "                <li><a href=\"#\">Annotations</a></li>\n" +
+    "\n" +
+    "            </ul> <!-- nav navbar-right -->\n" +
+    "    \n" +
+    "        </div><!-- /.navbar-collapse -->\n" +
+    "    </div><!-- /.container-fluid -->\n" +
+    "</nav>\n" +
+    "\n" +
+    "              \n" +
+    "");
 }]);
