09df85efb165a75de5ecca446ce966410ad11286
Raffaele Masotti <raffaele.masotti@gmail.com>
2014-05-03 16:11:13 +0200

    Minor changes Analytics and reply 2

--- app/src/Core/Analytics.service.js
+++ app/src/Core/Analytics.service.js
@@ -16,7 +16,7 @@ angular.module('Pundit2.Core')
     };
     var numSent = 0;
     var isSendRunning = false;
-    var update = undefined;
+    var update;
     var currentHits = analytics.options.hits;
 
     (function(i, s, o, g, r, a, m) {
@@ -32,7 +32,7 @@ angular.module('Pundit2.Core')
         a.onload = function() {
             analytics.log('GA async script loaded');
             ga = $window[analytics.options.globalTracker];
-        }
+        };
         m.parentNode.insertBefore(a, m);
     })($window, $document[0], 'script', 'http://www.google-analytics.com/analytics.js', analytics.options.globalTracker);
 
@@ -44,8 +44,6 @@ angular.module('Pundit2.Core')
     });
     ga('set', 'checkProtocolTask', function() {}); //HACK
 
-    // TODO RAF: updateHits con la h maiuscola? ;)
-    // REPLY: stavo già dormendo quando ho rinominato queste funzioni ^^
     var updateHits = function() {
         if (currentHits < analytics.options.hits){
             currentHits++;
@@ -64,6 +62,9 @@ angular.module('Pundit2.Core')
     // - se ci sono el in coda e/o hits < max, richiama se stesso (e rifara' una send e/o
     //   aggiornera' hits)
 
+    // REPLY: umh, mi torna piu' naturale pensarlo nel modo in cui l'ho strutturato ora,
+    // ci penso un attimo ..
+
     var updateHitsStart = function() {
         if (!angular.isDefined(update)) {
             update = $interval(function() {
@@ -80,75 +81,71 @@ angular.module('Pundit2.Core')
 
     var send = function() {
 
-        // TODO RAF: invece di avere un (o piu') if lungo come tutto il metodo, ribaltalo e
-        // copri tutti i casi di errore in cima, con dei return:
-        // if (!analytics.options... ) { return; }
-        // REPLY: devo adattarmi a questo stile
+        if (!analytics.options.doTracking){
+            return;
+        }
+        if (cache.events.length === 0){
+            isSendRunning = false;
+            return;
+        }
 
-        if (analytics.options.doTracking){
-            if (cache.events.length === 0){
-                isSendRunning = false;
-                return;
-            }
+        numSent++;
 
-            numSent++;
+        var currentEvent = cache.events.shift();
+        if (currentHits > 0){
 
-            var currentEvent = cache.events.shift();
-            if (currentHits > 0){
+            // TODO RAF: perche' un timer di 10ms qui??
+            // REPLY: la chiamata ricorsiva send() non funziona all'esterno
+            // di $timeout, non so perche' 
 
-                // TODO RAF: perche' un timer di 10ms qui??
-                // REPLY: la chiamata ricorsiva send() non funziona all'esterno
-                // di $timeout, non so perche' 
-
-                $timeout(function() {
-                    ga('send', {
-                        'hitType': 'event',
-                        'eventCategory': currentEvent.eventCategory,
-                        'eventAction': currentEvent.eventAction,
-                        'eventLabel': currentEvent.eventLabel,
-                        'eventValue': currentEvent.eventValue
-                    });
-                    analytics.log('Tracked ('+numSent+') event '+currentEvent.eventCategory+' ('+ currentEvent.eventAction +': '+ currentEvent.eventLabel +')');
-                    currentHits--;
-                    updateHitsStart();
-                    send();
-                }, 0);
-            } else {
-                $timeout(function() {
-                    ga('send', {
-                        'hitType': 'event',
-                        'eventCategory': currentEvent.eventCategory,
-                        'eventAction': currentEvent.eventAction,
-                        'eventLabel': currentEvent.eventLabel,
-                        'eventValue': currentEvent.eventValue
-                    });
-                    analytics.log('Tracked delayed ('+numSent+') event '+currentEvent.eventCategory+' ('+ currentEvent.eventAction +': '+ currentEvent.eventLabel +')');
-                    currentHits--;
-                    updateHitsStart();
-                    send();
-                }, analytics.options.bufferDelay);
-            }
+            $timeout(function() {
+                ga('send', {
+                    'hitType': 'event',
+                    'eventCategory': currentEvent.eventCategory,
+                    'eventAction': currentEvent.eventAction,
+                    'eventLabel': currentEvent.eventLabel,
+                    'eventValue': currentEvent.eventValue
+                });
+                analytics.log('Tracked ('+numSent+') event '+currentEvent.eventCategory+' ('+ currentEvent.eventAction +': '+ currentEvent.eventLabel +')');
+                currentHits--;
+                updateHitsStart();
+                send();
+            }, 0);
+        } else {
+            $timeout(function() {
+                ga('send', {
+                    'hitType': 'event',
+                    'eventCategory': currentEvent.eventCategory,
+                    'eventAction': currentEvent.eventAction,
+                    'eventLabel': currentEvent.eventLabel,
+                    'eventValue': currentEvent.eventValue
+                });
+                analytics.log('Tracked delayed ('+numSent+') event '+currentEvent.eventCategory+' ('+ currentEvent.eventAction +': '+ currentEvent.eventLabel +')');
+                currentHits--;
+                updateHitsStart();
+                send();
+            }, analytics.options.bufferDelay);
         }
     };
 
     analytics.track = function(category, action, label, value) {
 
-        // TODO: Puo' essere utile conoscere il file che genera l'errore?
-
-        function getErrorObject(){
-            try { throw Error('') } catch(err) { return err; }
-        }
-        var err = getErrorObject();
-        var caller_line = err.stack.split("\n")[4];
-        var index = caller_line.indexOf("at ");
-        var clean = caller_line.slice(index+2, caller_line.length);
 
         if (!analytics.options.doTracking){
             analytics.err("Tracking off"); //TODO: Da notificare: si o no?
             return;
         }
         if (!angular.isDefined(category) || !angular.isDefined(action)){
-            analytics.err("Category and Action are required " + caller_line);
+
+            // TODO: Puo' essere utile conoscere il file che genera l'errore?
+            // E nel caso, sarebbe il caso di trovare una posizione generica?
+            var err = function() {
+                try { throw Error(''); } catch(err) { return err; }
+            };
+            var currentErr = err();
+            var callerLine = currentErr.stack.split("\n")[4];
+
+            analytics.err("Category and Action are required " + callerLine);
             return;
         }
 
