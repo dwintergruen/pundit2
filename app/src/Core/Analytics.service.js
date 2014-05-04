0bedc6a81cc4136351484bb36a10599340920cd9
Raffaele Masotti <raffaele.masotti@gmail.com>
2014-05-04 04:16:19 +0200

    Analytics interval > timer and fix

--- app/src/Core/Analytics.service.js
+++ app/src/Core/Analytics.service.js
@@ -1,3 +1,5 @@
+/*jshint strict: false*/
+
 angular.module('Pundit2.Core')
 .constant('ANALYTICSDEFAULTS', {
     account: 'UA-50437894-1',
@@ -16,7 +18,7 @@ angular.module('Pundit2.Core')
     };
     var numSent = 0;
     var isSendRunning = false;
-    var update;
+    var updateHitsTimer;
     var currentHits = analytics.options.hits;
 
     (function(i, s, o, g, r, a, m) {
@@ -45,42 +47,21 @@ angular.module('Pundit2.Core')
     ga('set', 'checkProtocolTask', function() {}); //HACK
 
     var updateHits = function() {
-        if (currentHits < analytics.options.hits){
-            currentHits++;
-            analytics.log('Hits: '+currentHits);
-        } else {
-            updateHitsStop();
+        if (currentHits >= analytics.options.hits){
+            if (angular.isDefined(updateHitsTimer)){
+                $timeout.cancel( updateHitsTimer );
+                updateHitsTimer = undefined;    
+            }
             analytics.log('Stop! Hits: '+currentHits);
+            return;
         }
+        
+        currentHits++;
+        analytics.log('Hits: '+currentHits);
+        updateHitsTimer = $timeout(updateHits, analytics.options.bufferDelay);
     };
 
-    // TODO RAF: perche' li aggiorni con un interval invece di un timeout?
-    // forse sarebbe meglio se ci fosse un unico timeout che:
-    // - se ci sono elementi in coda, ne spedisce uno (come ora, con e senza timer),
-    //   fa partire un timer per aumentare hits tot ms dopo la chiamata (ricarica)
-    // - se non ci sono, fa solo partire un timer
-    // - se ci sono el in coda e/o hits < max, richiama se stesso (e rifara' una send e/o
-    //   aggiornera' hits)
-
-    // REPLY: umh, mi torna piu' naturale pensarlo nel modo in cui l'ho strutturato ora,
-    // ci penso un attimo ..
-
-    var updateHitsStart = function() {
-        if (!angular.isDefined(update)) {
-            update = $interval(function() {
-                updateHits();
-            }, analytics.options.bufferDelay);
-        }
-    };
-    var updateHitsStop = function() {
-        if (angular.isDefined(update)) {
-            $interval.cancel(update);
-            update = undefined;
-        }
-    };
-
-    var send = function() {
-
+    var sendHits = function() {
         if (!analytics.options.doTracking){
             return;
         }
@@ -92,47 +73,36 @@ angular.module('Pundit2.Core')
         numSent++;
 
         var currentEvent = cache.events.shift();
+        var sendNext = function(info) {
+            var logInfo = (info === 'delay' ? 'Tracked delayed' : 'Tracked');
+            analytics.log(logInfo+' ('+numSent+') event '+currentEvent.eventCategory+' ('+ currentEvent.eventAction +': '+ currentEvent.eventLabel +')');
+            
+            ga('send', {
+                'hitType': 'event',
+                'eventCategory': currentEvent.eventCategory,
+                'eventAction': currentEvent.eventAction,
+                'eventLabel': currentEvent.eventLabel,
+                'eventValue': currentEvent.eventValue
+            });
+            currentHits--;
+            if (!angular.isDefined(updateHitsTimer)){
+                updateHits();
+            }
+            sendHits();
+        };
+
         if (currentHits > 0){
-
-            // TODO RAF: perche' un timer di 10ms qui??
-            // REPLY: la chiamata ricorsiva send() non funziona all'esterno
-            // di $timeout, non so perche' 
-
-            $timeout(function() {
-                ga('send', {
-                    'hitType': 'event',
-                    'eventCategory': currentEvent.eventCategory,
-                    'eventAction': currentEvent.eventAction,
-                    'eventLabel': currentEvent.eventLabel,
-                    'eventValue': currentEvent.eventValue
-                });
-                analytics.log('Tracked ('+numSent+') event '+currentEvent.eventCategory+' ('+ currentEvent.eventAction +': '+ currentEvent.eventLabel +')');
-                currentHits--;
-                updateHitsStart();
-                send();
-            }, 0);
+            sendNext();
         } else {
             $timeout(function() {
-                ga('send', {
-                    'hitType': 'event',
-                    'eventCategory': currentEvent.eventCategory,
-                    'eventAction': currentEvent.eventAction,
-                    'eventLabel': currentEvent.eventLabel,
-                    'eventValue': currentEvent.eventValue
-                });
-                analytics.log('Tracked delayed ('+numSent+') event '+currentEvent.eventCategory+' ('+ currentEvent.eventAction +': '+ currentEvent.eventLabel +')');
-                currentHits--;
-                updateHitsStart();
-                send();
+                sendNext('delay');
             }, analytics.options.bufferDelay);
         }
     };
 
     analytics.track = function(category, action, label, value) {
-
-
         if (!analytics.options.doTracking){
-            analytics.err("Tracking off"); //TODO: Da notificare: si o no?
+            analytics.err('Tracking off'); //TODO: Da notificare: si o no?
             return;
         }
         if (!angular.isDefined(category) || !angular.isDefined(action)){
@@ -143,9 +113,9 @@ angular.module('Pundit2.Core')
                 try { throw Error(''); } catch(err) { return err; }
             };
             var currentErr = err();
-            var callerLine = currentErr.stack.split("\n")[4];
+            var callerLine = currentErr.stack.split('\n')[4];
 
-            analytics.err("Category and Action are required " + callerLine);
+            analytics.err('Category and Action are required ' + callerLine);
             return;
         }
 
@@ -157,8 +127,8 @@ angular.module('Pundit2.Core')
         });
 
         if (!isSendRunning){
-            send();
             isSendRunning = true;
+            sendHits();
         }
     };
 
