6c5bda3deef48015685742665f77ab051aec2601
Giacomo <donatigiacomo91@gmail.com>
2014-05-06 17:13:16 +0200

    add contextualMenu directory structure

---
+++ app/src/ContextualMenu/ContextualMenu.js
@@ -0,0 +1,7 @@
+/**
+ * @ngdoc module
+ * @name Pundit2.ContextualMenu
+ * @module Pundit2.ContextualMenu
+ * @description Something ..
+**/
+angular.module('Pundit2.ContextualMenu', []);
---
+++ app/src/ContextualMenu/ContextualMenu.service.js
@@ -0,0 +1,10 @@
+angular.module('Pundit2.ContextualMenu')
+.constant('CONTEXTUALMENUDEFAULT', {
+
+    isMenuVisible: true,
+    debug: true
+})
+.service('ContextualMenu', function(BaseComponent, CONTEXTUALMENUDEFAULT) {
+
+
+});
