f962e2c41c993a589abf5b1b86527246c1e925b1
Giacomo <donatigiacomo91@gmail.com>
2014-05-07 17:45:42 +0200

    try to append dropdown to created element

--- app/src/ContextualMenu/ContextualMenu.service.js
+++ app/src/ContextualMenu/ContextualMenu.service.js
@@ -9,6 +9,7 @@ angular.module('Pundit2.ContextualMenu')
 
     contextualMenu = new BaseComponent('ContextualMenu', CONTEXTUALMENUDEFAULT);
 
+    // TODO put in CONTEXTUALMENUDEFAULT
     var defaultContent = [
         { "text": "Action1", "href": "dashboard.html"},
         { "text": "Action2", "click": function(){
@@ -18,12 +19,15 @@ angular.module('Pundit2.ContextualMenu')
 
     var menuActions = [];
 
-    var menu;
+    // angular strap menu reference
+    var menu = null;
 
+    // build content to put inside menu
     var buildContent = function(){
         var content = angular.copy(defaultContent);
 
         for ( var i in menuActions ) {
+            // display only if showIf return true
             if ( menuActions[i].showIf() ) {
                 content.push({
                     "text": menuActions[i].label,
@@ -40,28 +44,52 @@ angular.module('Pundit2.ContextualMenu')
         var options = {scope: $rootScope.$new()};
         options.scope.content = buildContent();
         options.placement = ( typeof(position)!== 'undefined' ) ? position : contextualMenu.options.position;
+
+        // TODO need to cache?
+        //options.template = 'dropdown.tmpl.html';
+
         // build menu
-        menu = $dropdown(angular.element(element), options);
+        menu = $dropdown(element, options);
     };
 
-    contextualMenu.show = function(element, position){
-        init(element, position);       
+    // initialize the menu and show when it is ready
+    contextualMenu.show = function(position, x, y){
+
+        // TODO where position the <div> element ?
+        angular.element('.contexMenu-example-div')
+            .prepend("<div class='pnd-dropdown' style='position: absolute; left: " + x + "px; top: " + y + "px;'><div>");
+
+        init(angular.element('.pnd-dropdown'), position);       
         menu.$promise.then(menu.show);
     };
 
+    // hide and destroy the showed menu
     contextualMenu.hide = function(){
-        menu.hide();
-        menu.destroy();
+        if ( menu !== null) {
+            menu.hide();
+            menu.destroy();
+            angular.element('.pnd-dropdown').remove();
+            menu = null;
+        }
     };
 
+    // add passed action (use name as key to not produce duplicates) 
     contextualMenu.addAction = function(name, label, type, showIf, action){
-        menuActions.push({
-            name: name,
-            label: label,
-            type: type,
-            showIf: showIf,
-            action: action
+
+        var find = menuActions.some(function(el, index, array){
+            return angular.equals(name, el.name);
         });
+
+        if ( !find ) {
+            menuActions.push({
+                name: name,
+                label: label,
+                type: type,
+                showIf: showIf,
+                action: action
+            });            
+        }
+
     };
 
     contextualMenu.log('service run');
