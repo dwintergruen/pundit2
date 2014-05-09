7114c931809b3c7a5509780a8a0e2198b58bc285
Raffaele Masotti <raffaele.masotti@gmail.com>
2014-05-09 11:59:03 +0200

    AnnotationSidebar watch controller > directive

--- app/src/AnnotationSidebar/AnnotationSidebar.ctrl.js
+++ app/src/AnnotationSidebar/AnnotationSidebar.ctrl.js
@@ -2,34 +2,7 @@
 
 angular.module('Pundit2.AnnotationSidebar')
 .controller('AnnotationSidebarCtrl', function($scope, AnnotationSidebar) {
-    var body = angular.element('body');
-    body.css({
-        'margin-right' : AnnotationSidebar.getWidth()
-    });
-
-    var container = angular.element('.pnd-annotation-sidebar-container');
-    container.css({
-        'width' : AnnotationSidebar.getWidth()
-    });
-
-    $scope.isAnnotationSidebarExpanded = AnnotationSidebar.isAnnotationSidebarExpanded();
-
-    $scope.$watch(function() {
-        return AnnotationSidebar.isAnnotationSidebarExpanded();
-    }, function(currentState) {
-        $scope.isAnnotationSidebarExpanded = currentState;
-    });
-
-    $scope.$watch(function() {
-        return AnnotationSidebar.getWidth();
-    }, function(width) {
-        body.css({
-            'margin-right' : width
-        });
-        container.css({
-            'width' : width
-        });
-    });
+    // ...
 
     AnnotationSidebar.log('Controller Run');
 });
--- app/src/AnnotationSidebar/AnnotationSidebar.dir.js
+++ app/src/AnnotationSidebar/AnnotationSidebar.dir.js
@@ -1,12 +1,40 @@
 angular.module('Pundit2.AnnotationSidebar')
-.directive('annotationSidebar', function() {
+.directive('annotationSidebar', function(AnnotationSidebar) {
     return {
         restrict: 'E',
         scope: {},
         templateUrl: "src/AnnotationSidebar/AnnotationSidebar.dir.tmpl.html",
         controller: "AnnotationSidebarCtrl",
-        link: function(/* scope, el, attrs, ctrl */) {
-            // Stuff to do on link? read some conf?
+        link: function(scope, element, attrs) {
+
+            var body = angular.element('body');
+            body.css({
+                'margin-right': AnnotationSidebar.getWidth()
+            });
+
+            var container = angular.element('.pnd-annotation-sidebar-container');
+            container.css({
+                'width': AnnotationSidebar.getWidth()
+            });
+
+
+            scope.$watch(function() {
+                return AnnotationSidebar.isAnnotationSidebarExpanded();
+            }, function(currentState) {
+                scope.isAnnotationSidebarExpanded = currentState;
+            });
+
+            scope.$watch(function() {
+                return AnnotationSidebar.getWidth();
+            }, function(width) {
+                body.css({
+                    'margin-right': width
+                });
+                container.css({
+                    'width': width
+                });
+            });
+
         }
     };
 });
