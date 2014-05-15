3414f2655702c6973211d41495d7bd990ceda000
Giacomo <donatigiacomo91@gmail.com>
2014-05-15 18:03:04 +0200

    item display

--- app/src/Item/Item.ctrl.js
+++ app/src/Item/Item.ctrl.js
@@ -1,7 +1,9 @@
 angular.module('Pundit2.Item')
-.controller('ItemCtrl', function($scope, ItemsExchange, TypesHelper) {
+.controller('ItemCtrl', function($scope, ItemsExchange, TypesHelper, Preview) {
 
+    // get item by uri (passed as directive param)
     $scope.item = ItemsExchange.getItemByUri($scope.uri);
+    // get item type label
     $scope.itemTypeLabel = TypesHelper.getLabel($scope.item.type[0]);
 
     $scope.onItemMouseOver = function(){
@@ -9,7 +11,7 @@ angular.module('Pundit2.Item')
     };
 
     $scope.onItemMouseLeave = function(){
-
+        Preview.hideDashboardPreview();
     };
 
 });
