5ea1317c02e4e73dcd12cbe26ef569dca5aa4203
SimoneSabba <sabbatini.simone@gmail.com>
2014-05-21 13:11:10 +0200

    added calendar popover

--- app/src/ResourcePanel/ResourcePanel.service.js
+++ app/src/ResourcePanel/ResourcePanel.service.js
@@ -8,6 +8,11 @@ angular.module('Pundit2.ResourcePanel')
     var state = {};
 
     state.selectors = ['freebase', 'dbpedia', 'korbo'];
+    /*
+    *
+    * LITERAL POPOVER METHOD
+    *
+    */
 
     // create div literalAnchor where literal append popover
     angular.element("[data-ng-app='Pundit2']")
@@ -61,14 +66,14 @@ angular.module('Pundit2.ResourcePanel')
         }
 
         // if click a different popover, hide the shown popover and show the clicked one
-        if (state.popoverLiteral !== null && state.popoverLiteral.clickTarget !== target) {
+        else if (state.popoverLiteral !== null && state.popoverLiteral.clickTarget !== target) {
             resourcePanel.hideLiteral();
             state.popoverLiteral = initPopoverLiteral(x, y, content, target);
             state.popoverLiteral.$promise.then(state.popoverLiteral.show);
         }
 
         // if no popover is shown, just show it
-        if (state.popoverLiteral === null) {
+        else if (state.popoverLiteral === null) {
             state.popoverLiteral = initPopoverLiteral(x, y, content, target);
             state.popoverLiteral.$promise.then(state.popoverLiteral.show);
          }
@@ -95,6 +100,83 @@ angular.module('Pundit2.ResourcePanel')
     resourcePanel.getLiteral = function() {
         return state.literal;
     };
+
+    /*
+     *
+     * CALENDAR POPOVER METHOD
+     *
+     *
+     */
+    // create div literalAnchor where literal append popover
+    angular.element("[data-ng-app='Pundit2']")
+        .prepend("<div class='pnd-calendar-popover-calendarAnchor' style='position: absolute; left: -500px; top: -500px;'><div>");
+    state.calendarAnchor = angular.element('.pnd-calendar-popover-calendarAnchor');
+
+    state.popoverCalendar = null;
+    // initialize calendar popover
+    var initPopoverCalendar = function(x, y, date, target){
+        // move literalAnchor to correct position
+        state.calendarAnchor.css({
+            left: x,
+            top: y
+        });
+
+        // handle save a new popoverLiteral
+        state.popoverOptions.scope.save = function() {
+            resourcePanel.saveDate(this.selectedDate);
+            state.resourcePromise.resolve(this.selectedDate);
+        };
+
+        // close popoverLiteral popover without saving
+        state.popoverOptions.scope.cancel = function() {
+            resourcePanel.hideCalendar();
+        };
+
+        state.popoverOptions.placement = 'bottom';
+        state.popoverOptions.template = 'src/ResourcePanel/popoverCalendar.tmpl.html';
+        state.popoverCalendar = $popover(state.calendarAnchor, state.popoverOptions);
+        state.popoverCalendar.clickTarget = target;
+        return state.popoverCalendar;
+    };
+
+    resourcePanel.showPopoverCalendar = function(x, y, date, target){
+
+        // if no popover is shown, just show it
+        if (state.popoverCalendar === null) {
+            state.popoverCalendar = initPopoverCalendar(x, y, date, target);
+            state.popoverCalendar.$promise.then(state.popoverCalendar.show);
+        }
+
+        // if click the same popover, toggle it
+        else if (state.popoverCalendar !== null && state.popoverCalendar.clickTarget === target) {
+            state.popoverCalendar.$promise.then(state.popoverCalendar.toggle);
+        }
+
+        // if click a different popover, hide the shown popover and show the clicked one
+        else if (state.popoverCalendar !== null && state.popoverCalendar.clickTarget !== target) {
+            resourcePanel.hideCalendar();
+            state.popoverCalendar = initPopoverCalendar(x, y, date, target);
+            state.popoverCalendar.$promise.then(state.popoverCalendar.show);
+        }
+
+        state.resourcePromise = $q.defer();
+        return state.resourcePromise.promise;
+    };
+
+    resourcePanel.hideCalendar = function(){
+
+        if(state.popoverCalendar === null){
+            return;
+        }
+        state.popoverCalendar.hide();
+        state.popoverCalendar.destroy();
+        state.popoverCalendar = null;
+    };
+
+    resourcePanel.saveDate = function(date) {
+        console.log("date selected", date);
+        resourcePanel.hideCalendar();
+    }
 
     return resourcePanel;
 });
