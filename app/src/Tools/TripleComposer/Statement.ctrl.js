67d8c35cdbe3bf3bc46b5c15eb50767deb79795e
Giacomo <donatigiacomo91@gmail.com>
2014-05-27 16:48:21 +0200

    triple composer show resource panel

--- app/src/Tools/TripleComposer/Statement.ctrl.js
+++ app/src/Tools/TripleComposer/Statement.ctrl.js
@@ -1,7 +1,8 @@
 angular.module('Pundit2.TripleComposer')
-.controller('StatementCtrl', function($scope) {
+.controller('StatementCtrl', function($scope, TypesHelper, ResourcePanel) {
 
     $scope.subjectLabel = '';
+    $scope.subjectTypeLabel = '';
     $scope.subjectFound = false;
     $scope.subjectSearch = "search subject";
     $scope.subjectIcon = 'pnd-icon-search'; //'pnd-icon-times'
@@ -12,11 +13,66 @@ angular.module('Pundit2.TripleComposer')
     $scope.predicateIcon = 'pnd-icon-search'; //'pnd-icon-times'
 
     $scope.objectLabel = '';
+    $scope.objectTypeLabel = '';
     $scope.objectFound = false;
     $scope.objectSearch = "search object";
     $scope.objectIcon = 'pnd-icon-search'; //'pnd-icon-times'
 
-    $scope.click = function(){
-        console.log('inputclick');
+    $scope.wipeStatement = function(){
+        $scope.subjectLabel = '';
+        $scope.subjectTypeLabel = '';
+        $scope.subjectFound = false;
+        $scope.subjectSearch = "search subject";
+        $scope.subjectIcon = 'pnd-icon-search'; 
+
+        $scope.predicateLabel = '';
+        $scope.predicateFound = false;
+        $scope.predicateSearch = "search predicate";
+        $scope.predicateIcon = 'pnd-icon-search'; 
+
+        $scope.objectLabel = '';
+        $scope.objectTypeLabel = '';
+        $scope.objectFound = false;
+        $scope.objectSearch = "search object";
+        $scope.objectIcon = 'pnd-icon-search'; 
     };
+
+    $scope.onClickSubject = function($event){
+        ResourcePanel.showItemsForSubject($event.pageX, $event.pageY, [], $event.target).then(function(item){
+            $scope.subjectFound = true;
+            $scope.subjectLabel = item.label;
+            $scope.subjectTypeLabel = TypesHelper.getLabel(item.type[0]);
+        });
+    };
+
+    $scope.onClickPredicate = function($event){
+        ResourcePanel.showProperties($event.pageX, $event.pageY, [], $event.target).then(function(item){
+            $scope.predicateFound = true;
+            $scope.predicateLabel = item.label;
+        });
+    };
+
+    $scope.onClickObject = function($event){
+        ResourcePanel.showItemsForObject($event.pageX, $event.pageY, [], $event.target).then(function(item){
+            $scope.objectFound = true;
+            $scope.objectLabel = item.label;
+            $scope.objectTypeLabel = TypesHelper.getLabel(item.type[0]);
+        });
+    };
+
+    $scope.onClickObjectCalendar = function($event){
+        ResourcePanel.showPopoverCalendar($event.pageX, $event.pageY, undefined, $event.target).then(function(date){
+
+        });
+    };
+
+    $scope.onClickObjectLiteral = function($event){
+        ResourcePanel.showPopoverLiteral($event.pageX, $event.pageY, '', $event.target).then(function(text){
+            $scope.objectFound = true;
+            $scope.objectLabel = text;
+            $scope.objectTypeLabel = 'literal';
+        });
+    };
+
+
 });
