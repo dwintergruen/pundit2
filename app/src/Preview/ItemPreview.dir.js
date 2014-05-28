45a4b4ac34bd7f520943d035f71636e3da3a13d9
Simone Fonda <fonda@netseven.it>
2014-05-28 22:52:55 +0200

    Preview refactored to use ItemPreview directive

---
+++ app/src/Preview/ItemPreview.dir.js
@@ -0,0 +1,11 @@
+angular.module('Pundit2.Preview')
+    .directive('itemPreview', function() {
+        return {
+            restrict: 'E',
+            scope: {
+                uri: '@'
+            },
+            templateUrl: "src/Preview/ItemPreview.dir.tmpl.html",
+            controller: 'ItemPreviewCtrl'
+        };
+    });
