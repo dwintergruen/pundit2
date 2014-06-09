576048281e621b32dd11eb7f0bcb1f902ba5d521
Raffaele Masotti <raffaele.masotti@gmail.com>
2014-06-09 12:32:01 +0200

    AnnotationSidebar some tries e2e

---
+++ test/e2e/annotationSidebar.e2e.test.js
@@ -0,0 +1,19 @@
+describe("AnnotationSidebar interaction", function() {
+    var p = protractor.getInstance();
+
+    it('should toggle the sidebar', function() {
+
+        // console.log("prima get");
+
+        // p.get('/app/examples/annotationSidebar.html');
+
+        // p.findElements(protractor.By.css('.pnd-annotation-sidebar-container')).then(function(elements) {
+        //     console.log("dentro find");            
+        //     expect(elements.length).toBe(1);
+        // });
+
+        // p.findElement(protractor.By.css('.pnd-annotation-sidebar-toggle-btn')).click();
+
+    });
+
+});
