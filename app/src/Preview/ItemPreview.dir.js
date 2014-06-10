4e9c20bce79a3444526383b46186030e05b582ed
SimoneSabba <sabbatini.simone@gmail.com>
2014-06-10 14:14:57 +0200

    minor fix

--- app/src/Preview/ItemPreview.dir.js
+++ app/src/Preview/ItemPreview.dir.js
@@ -6,11 +6,6 @@ angular.module('Pundit2.Preview')
                 uri: '@'
             },
             templateUrl: "src/Preview/ItemPreview.dir.tmpl.html",
-            controller: 'ItemPreviewCtrl',
-
-              link: function postLink(scope, elem, attrs) {
-                  scope.a = angular.element(elem).find('li.pnd-preview-single-type');
-              }
-
+            controller: 'ItemPreviewCtrl'
         };
     });
