37468cac51c63374316c7c594626a03b1d5fb81a
SimoneSabba <sabbatini.simone@gmail.com>
2014-06-11 12:53:14 +0200

    fixed preview types

--- app/src/Preview/ItemPreview.ctrl.js
+++ app/src/Preview/ItemPreview.ctrl.js
@@ -1,5 +1,5 @@
 angular.module('Pundit2.Preview')
-    .controller('ItemPreviewCtrl', function($scope, TypesHelper, ItemsExchange, $element) {
+    .controller('ItemPreviewCtrl', function($scope, TypesHelper, ItemsExchange, $element, Preview) {
 
         // get the label of a type from his uri
         $scope.getTypeLabel = function(uri) {
@@ -10,44 +10,69 @@ angular.module('Pundit2.Preview')
             // TODO: special initialization for certain kind of items, like image fragments?
             $scope.item = ItemsExchange.getItemByUri($scope.uri);
 
+            if(Preview.getItemDashboardSticky() !== null && Preview.getItemDashboardSticky().uri === $scope.uri){
+                $scope.isSticky = true;
+            } else {
+                $scope.isSticky = false;
+            }
+
+
         });
 
         $scope.$watch(function() {
-            //return angular.element($element).find('ul.pnd-tab-header').css('width');
             return angular.element($element).find('li.pnd-preview-single-type').css('width');
         }, function() {
 
-            // get <li> elements where types are shown
-            var liList = angular.element($element).find('li.pnd-preview-single-type');
+            var liList;
 
-            // get <ul> width containing types
-            var luWidth = parseInt(angular.element('.pnd-preview-item-types-ul').css('width'), 10);
+            if($scope.isSticky){
 
-            // get height (with margin) of single <li> element
-            var heightLiSingle = angular.element(liList[0]).outerHeight(true);
+                $scope.typeHiddenPresent = $scope.prev;
 
-            // check if all types fit the ul width
-            checkFitTypes(liList, luWidth);
+            }else{
 
-            // get div where types are shown
-            var divTypes = angular.element('div.pnd-preview-item-types');
+                //liList = angular.element('ul.pnd-preview-item-types-ul li:not(.pnd-is-sticky)');
+                liList = angular.element($element).find('div.pnd-preview-item-types').children('ul.pnd-preview-item-types-ul').children('li:not(.pnd-is-sticky)');
 
-            // get padding div below type. It need to calculate the right height for div where types are shown
-            var divPrevImg = parseInt(angular.element('.pnd-preview-item-image').css('padding-top'), 10);
+                // get <ul> width containing types
+                //var luWidth = parseInt(angular.element('.pnd-preview-item-types-ul').css('width'), 10);
+                var luWidth = parseInt(angular.element($element).find('div.pnd-preview-item-types').children('ul.pnd-preview-item-types-ul').css('width'), 10);
 
-            // set div types height
-            var h = heightLiSingle + divPrevImg;
-            divTypes.css({
-                'height' : h
-            });
+                // get height (with margin) of single <li> element
+                var heightLiSingle = angular.element(liList[0]).outerHeight(true);
 
-            $scope.heigthTypesDiv = h;
+                // get div where types are shown
+                var divTypes = angular.element('div.pnd-preview-item-types');
+
+                // get padding div below type. It need to calculate the right height for div where types are shown
+                var prevDivPadding = 0;
+                if(angular.element('.pnd-preview-item-image').length > 0){
+                    prevDivPadding = parseInt(angular.element('.pnd-preview-item-image').css('padding-top'), 10);
+                } else if(angular.element('.pnd-preview-item-description').length > 0){
+                    prevDivPadding = parseInt(angular.element('.pnd-preview-item-description').css('padding-top'), 10);
+                    console.log(prevDivPadding);
+                }
+        //angular.element($element).find('div.pnd-preview-item-types').children('.pnd-preview-item-image')
+
+                // set div types height
+                var h = heightLiSingle + prevDivPadding;
+                divTypes.css({
+                    'height' : h
+                });
+
+                $scope.heigthTypesDiv = h;
+                checkFitTypes(liList, luWidth);
+
+                var li = angular.element('li.pnd-preview-single-type');
+
+            }
 
         });
 
         // check if there is at least a type that doesn't fit in <ul> width
         // set a flag in itemPreview scope
         var checkFitTypes = function(typeList, ulWidth) {
+            $scope.prev = $scope.typeHiddenPresent;
             var tmpWidth = 0,
             // offset for caret icon
                 offset = 30,
