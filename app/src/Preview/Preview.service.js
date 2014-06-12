9c655e812a1900a480bf37e85944cd6c5e51f985
SimoneSabba <sabbatini.simone@gmail.com>
2014-06-12 16:23:35 +0200

    added preview service docs

--- app/src/Preview/Preview.service.js
+++ app/src/Preview/Preview.service.js
@@ -88,6 +88,17 @@ angular.module('Pundit2.Preview')
     clientDashboardTabTitle: "Preview"
 
 })
+
+/**
+ * @ngdoc service
+ * @name Preview
+ * @module Pundit2.Preview
+ * @description
+ *
+ * Handles the preview of an item: given an item, show/hide a preview of some details of the item.
+ * It is possible set an item as sticky or not, and check if a given item is set as sticky or not.
+ *
+ */
 .service('Preview', function(BaseComponent, PREVIEWDEFAULTS) {
 
     var preview = new BaseComponent('Preview', PREVIEWDEFAULTS);
@@ -99,37 +110,122 @@ angular.module('Pundit2.Preview')
         typesHidden: null
         };
 
+    /**
+     * @ngdoc method
+     * @name Preview#getWelcomeHeaderMessage
+     * @module Pundit2.Preview
+     * @function
+     *
+     * @description
+     * Return the welcome message that will be shown in the header of preview panel when no preview is shown.
+     *
+     * @return {string} header welcome message set in the configuration of Preview module. See {@link #!/api/punditConfig/object/modules#Preview.welcomeHeaderMessage here} for details
+     *
+     */
     preview.getWelcomeHeaderMessage = function() {
         return preview.options.welcomeHeaderMessage;
     };
 
+    /**
+     * @ngdoc method
+     * @name Preview#getWelcomeBodyMessage
+     * @module Pundit2.Preview
+     * @function
+     *
+     * @description
+     * Return the welcome message that will be shown in the body of preview panel when no preview is shown.
+     *
+     * @return {string} body welcome message set in the configuration of Preview module. See {@link #!/api/punditConfig/object/modules#Preview.welcomeBodyMessage here} for details
+     *
+     */
     preview.getWelcomeBodyMessage = function() {
         return preview.options.welcomeBodyMessage;
     };
 
-    // show item preview in dashboard panel
+    /**
+     * @ngdoc method
+     * @name Preview#showDashboardPreview
+     * @module Pundit2.Preview
+     * @function
+     *
+     * @description
+     * Show a preview of the given item in the chosen panel. The panel where preview will be show is that set in configuration object. See {@link #!/api/punditConfig/object/modules#Preview.clientDashboardPanel here} for details
+     * Preview will show the follow details of an item:
+     * * label
+     * * types
+     * * description (if present)
+     * * image (if present)
+     *
+     * @param {Object} Item to show in preview panel.
+     *
+     */
     preview.showDashboardPreview = function(item) {
         state.itemDashboardPreview = item;
     };
 
-    // get current item shown in preview
+
+    /**
+     * @ngdoc method
+     * @name Preview#getItemDashboardPreview
+     * @module Pundit2.Preview
+     * @function
+     *
+     * @description
+     * Get current item shown in preview preview
+     *
+     * @return {Object} Current item shown in preview panel
+     *
+     */
     preview.getItemDashboardPreview = function() {
         return state.itemDashboardPreview;
     };
 
-    // set an item as sticky
+    /**
+     * @ngdoc method
+     * @name Preview#setItemDashboardSticky
+     * @module Pundit2.Preview
+     * @function
+     *
+     * @description
+     * Set an item as sticky.
+     *
+     * In this way, if there is not a current item shown in preview, the panel in dashboard will not be empty but the sticky item will be shown on it.
+     *
+     * @param {Object} Item to set as sticky.
+     *
+     */
     preview.setItemDashboardSticky = function(item) {
         state.itemDashboardSticky = item;
     };
 
-    // return current sticky item
+    /**
+     * @ngdoc method
+     * @name Preview#getItemDashboardSticky
+     * @module Pundit2.Preview
+     * @function
+     *
+     * @description
+     * Get the current stitky item
+     *
+     * @return {Object} Current item set as sticky
+     *
+     */
     preview.getItemDashboardSticky = function() {
         return state.itemDashboardSticky;
     };
 
-    // reset dashboard preview
-    // clear both item dashoboard and sticky item
-    // in this way preview panel will be empty and will be shown welcome message
+    /**
+     * @ngdoc method
+     * @name Preview#clearItemDashboardSticky
+     * @module Pundit2.Preview
+     * @function
+     *
+     * @description
+     * Reset dashboard preview, cleaning both item show in preview and sticky item.
+     *
+     * In this way, preview panel will be empty and will be shown welcome message
+     *
+     */
     preview.clearItemDashboardSticky = function() {
         state.itemDashboardSticky = null;
         state.itemDashboardPreview = null;
@@ -137,6 +233,17 @@ angular.module('Pundit2.Preview')
 
     // if no item is sticky, show empty preview
     // otherwise show the sticky item
+    /**
+     * @ngdoc method
+     * @name Preview#hideDashboardPreview
+     * @module Pundit2.Preview
+     * @function
+     *
+     * @description
+     * Hide current item preview. If there is a sticky item, it will be shown in preview panel,
+     * otherwise preview panel will be empty and will be shown welcome message
+     *
+     */
     preview.hideDashboardPreview = function(){
 
         if(state.itemDashboardSticky === null ) {
@@ -148,6 +255,21 @@ angular.module('Pundit2.Preview')
 
     // return true if an item is the sticky item
     // if no item is given, check if current item preview is the sticky item
+    /**
+     * @ngdoc method
+     * @name Preview#isStickyItem
+     * @module Pundit2.Preview
+     * @function
+     *
+     * @description
+     * Check if the passed item it's the current sticky item or not.
+     *
+     * In this way, if there is not a current item shown in preview, the panel in dashboard will not be empty but the sticky item will be shown on it.
+     *
+     * @param {Object} Item to check if it is the sticky item.
+     * @return {boolean} `true` if the passed item is the current sticky item, `false` otherwise
+     *
+     */
     preview.isStickyItem = function(item) {
         var itemToCheck;
 
