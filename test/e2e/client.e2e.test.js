b057e79b1a34907ca2ccd684edd6484d3744ddca
Giacomo <donatigiacomo91@gmail.com>
2014-06-24 16:11:03 +0200

    init to build client e2e test (add file)

---
+++ test/e2e/client.e2e.test.js
@@ -0,0 +1,33 @@
+ddescribe("Client interaction", function() {
+
+    var httpMock = function() {
+        angular.module('httpBackendMock', ['ngMockE2E'])
+            .run(function($httpBackend) {
+
+                $httpBackend.whenGET(/.*/).passThrough();
+                $httpBackend.whenJSONP(/.*/).passThrough();
+
+            });
+    };
+
+    var p = protractor.getInstance();
+
+    beforeEach(function(){
+        p.addMockModule('httpBackendMock', httpMock);
+        p.get('/app/examples/client-TEST.html');
+    });
+
+    afterEach(function() {
+        p.removeMockModule('httpBackendMock');
+    });
+
+    it("should correctly show item", function(){
+
+        p.findElement(protractor.By.css("[data-ng-app='Pundit2']")).then(function(item) {
+            console.log("compleate loading ????");
+            p.sleep(30000);
+        });
+
+    });
+
+});
