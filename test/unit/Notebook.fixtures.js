64481c42f96672e282b647893889a89dc75db613
SimoneSabba <sabbatini.simone@gmail.com>
2014-06-26 16:05:58 +0200

    added some notebook communication service unit tests

--- test/unit/Notebook.fixtures.js
+++ test/unit/Notebook.fixtures.js
@@ -33,3 +33,62 @@ testNotebooks.myNotebook = {
         "http://www.w3.org/2000/01/rdf-schema#label": [{value: "myNotebook", type: "literal"}]
     }
 };
+
+testNotebooks.notebookPrivate = {
+    "http://purl.org/pundit/demo-cloud-server/notebook/88e4c1e8": {
+        "http://open.vocab.org/terms/visibility": [{value: "private", type: "literal"}],
+        "http://purl.org/dc/elements/1.1/creator": [{value: "Mario Bros", type: "literal"}],
+        "http://purl.org/pundit/ont/ao#id": [{value: "88e4c1e8", type: "literal"}],
+        "http://purl.org/pundit/ont/ao#includes": [{
+            value: "http://purl.org/pundit/demo-cloud-server/annotation/testannid99",
+            type: "uri"
+        }],
+        "http://www.w3.org/1999/02/22-rdf-syntax-ns#type": [{
+            value: "http://purl.org/pundit/ont/ao#Notebook",
+            type: "uri"
+        }],
+        "http://www.w3.org/2000/01/rdf-schema#label": [{value: "notebook1", type: "literal"}]
+    }
+};
+
+/*testNotebooks.notebookPrivate = {
+    created: "2014-06-24T10:53:09",
+    creator: "http://purl.org/pundit/demo-cloud-server/user/111111",
+    creatorName: "Mario Bros",
+    id: "88e4c1e8",
+    includes: ['ann01', 'ann02'],
+    label: "notebook1",
+    type: ['http://purl.org/pundit/ont/ao#Notebook'],
+    uri: "http://purl.org/pundit/demo-cloud-server/notebook/88e4c1e8",
+    visibility: "private"
+};*/
+
+testNotebooks.notebookPublic = {
+    "http://purl.org/pundit/demo-cloud-server/notebook/123456789": {
+        "http://open.vocab.org/terms/visibility": [{value: "public", type: "literal"}],
+        "http://purl.org/dc/elements/1.1/creator": [{value: "Godzilla", type: "literal"}],
+        "http://purl.org/pundit/ont/ao#id": [{value: "123456789", type: "literal"}],
+        "http://purl.org/pundit/ont/ao#includes": [{
+            value: "http://purl.org/pundit/demo-cloud-server/annotation/testannid98",
+            type: "uri"
+        }],
+        "http://www.w3.org/1999/02/22-rdf-syntax-ns#type": [{
+            value: "http://purl.org/pundit/ont/ao#Notebook",
+            type: "uri"
+        }],
+        "http://www.w3.org/2000/01/rdf-schema#label": [{value: "notebook2", type: "literal"}]
+    }
+};
+
+/*
+testNotebooks.notebookPublic = {
+    created: "2014-06-24T10:53:09",
+    creator: "http://purl.org/pundit/demo-cloud-server/user/111111",
+    creatorName: "Godzilla",
+    id: "123456789",
+    includes: ['ann01', 'ann02'],
+    label: "notebook1",
+    type: ['http://purl.org/pundit/ont/ao#Notebook'],
+    uri: "http://purl.org/pundit/demo-cloud-server/notebook/123456789",
+    visibility: "public"
+};*/
--- test/unit/NotebookCommunication.service.unit.test.js
+++ test/unit/NotebookCommunication.service.unit.test.js
@@ -6,11 +6,16 @@ describe('Notebook Communication service', function() {
         NameSpace,
         $compile,
         Notebook,
-        NotebookExchange;
+        NotebookExchange,
+        MyPundit;
+
+    var userLoggedIn = {
+        loginStatus: 1
+    };
 
     beforeEach(module('Pundit2'));
 
-    beforeEach(inject(function($injector, _$rootScope_, _$httpBackend_, _$compile_, _Notebook_, _NotebookExchange_){
+    beforeEach(inject(function($injector, _$rootScope_, _$httpBackend_, _$compile_, _Notebook_, _NotebookExchange_, _MyPundit_){
         NotebookCommunication = $injector.get('NotebookCommunication');
         NameSpace = $injector.get('NameSpace');
         $rootScope = _$rootScope_;
@@ -18,23 +23,14 @@ describe('Notebook Communication service', function() {
         $compile = _$compile_;
         Notebook = _Notebook_;
         NotebookExchange = _NotebookExchange_;
+        MyPundit = _MyPundit_;
 
     }));
 
     it("should create a new notebook", function() {
 
         var name = 'myNotebook';
-        var userLoggedIn = {
-            loginStatus: 1,
-            id: "myFakeId",
-            uri: "http://myUri.fake",
-            openid: "http://myOpenId.fake",
-            firstName: "Mario",
-            lastName: "Rossi",
-            fullName: "Mario Rossi",
-            email: "mario@rossi.it",
-            loginServer: "http:\/\/demo-cloud.as.thepund.it:8080\/annotationserver\/login.jsp"
-        };
+
 
         var header = {"Accept":"application/json","Content-Type":"application/json;charset=UTF-8;"};
         var data = {"NotebookName": 'myNotebook'};
@@ -51,7 +47,7 @@ describe('Notebook Communication service', function() {
             .expectPOST(NameSpace.get('asNB'), data, header)
             .respond({"NotebookID": testId});
 
-        // catch http request for get notebook metadata, called when bu Notebook.factory when a notebook is created
+        // catch http request for get notebook metadata, called by Notebook.factory when a notebook is created
         $httpBackend
             .whenGET(NameSpace.get('asNBMeta', {id: testId}))
             .respond(testNotebooks.myNotebook);
@@ -72,6 +68,263 @@ describe('Notebook Communication service', function() {
 
         $httpBackend.flush();
         $rootScope.$digest();
+
+    });
+
+    it("should get my notebook", function() {
+
+        // http mock for login
+        $httpBackend.whenGET(NameSpace.get('asUsersCurrent')).respond(userLoggedIn);
+
+        var data = {
+                NotebookIDs: []
+            };
+
+        // get info about mocked public notebook
+        var nbPublic = Object.keys(testNotebooks.notebookPublic)[0];
+        var nbPublicVisibility = Object.keys(testNotebooks.notebookPublic[nbPublic])[0];
+        var nbPublicID = Object.keys(testNotebooks.notebookPublic[nbPublic])[2];
+        var nbPublicType = Object.keys(testNotebooks.notebookPublic[nbPublic])[4];
+        var nbPublicLabel = Object.keys(testNotebooks.notebookPublic[nbPublic])[5];
+
+        // get info about mocked private notebook
+        var nbPrivate = Object.keys(testNotebooks.notebookPrivate)[0];
+        var nbPrivateVisibility = Object.keys(testNotebooks.notebookPrivate[nbPrivate])[0];
+        var nbPrivateID = Object.keys(testNotebooks.notebookPrivate[nbPrivate])[2];
+        var nbPrivateType = Object.keys(testNotebooks.notebookPrivate[nbPrivate])[4];
+        var nbPrivateLabel = Object.keys(testNotebooks.notebookPrivate[nbPrivate])[5];
+
+
+        data.NotebookIDs.push(testNotebooks.notebookPrivate[nbPrivate][nbPrivateID][0].value);
+        data.NotebookIDs.push(testNotebooks.notebookPublic[nbPublic][nbPublicID][0].value);
+
+        $httpBackend.whenGET(NameSpace.get('asNBOwned')).respond(data);
+
+        $httpBackend
+            .whenGET(NameSpace.get('asNBMeta', {id: "88e4c1e8"}))
+            .respond(testNotebooks.notebookPrivate);
+
+        $httpBackend
+            .whenGET(NameSpace.get('asNBMeta', {id: "123456789"}))
+            .respond(testNotebooks.notebookPublic);
+
+        var promiseValue;
+
+        // get login
+        var promise = MyPundit.login();
+
+        promise.then(function(value){
+            expect(value).toBe(true);
+            // when user is logged in, get my notebooks
+            var p = NotebookCommunication.getMyNotebooks();
+
+            p.then(function(nb) {
+                expect(nb.length).toBe(2);
+
+                expect(nb[0].id).toBe(testNotebooks.notebookPrivate[nbPrivate][nbPrivateID][0].value);
+                expect(nb[0].visibility).toBe(testNotebooks.notebookPrivate[nbPrivate][nbPrivateVisibility][0].value);
+                expect(nb[0].label).toBe(testNotebooks.notebookPrivate[nbPrivate][nbPrivateLabel][0].value);
+                expect(nb[0].type[0]).toBe(testNotebooks.notebookPrivate[nbPrivate][nbPrivateType][0].value);
+
+                expect(nb[1].id).toBe(testNotebooks.notebookPublic[nbPublic][nbPublicID][0].value);
+                expect(nb[1].visibility).toBe(testNotebooks.notebookPublic[nbPublic][nbPublicVisibility][0].value);
+                expect(nb[1].label).toBe(testNotebooks.notebookPublic[nbPublic][nbPublicLabel][0].value);
+                expect(nb[1].type[0]).toBe(testNotebooks.notebookPublic[nbPublic][nbPublicType][0].value);
+
+
+            });
+
+        });
+        $httpBackend.flush();
+
+
+    });
+
+    it("should get current notebook", function() {
+
+        var currentNotebook = {
+            NotebookID: "123456"
+        };
+
+        // http mock for login
+        $httpBackend.whenGET(NameSpace.get('asUsersCurrent')).respond(userLoggedIn);
+        $httpBackend.whenGET(NameSpace.get('asNBCurrent')).respond(currentNotebook);
+
+        MyPundit.login().then(function(){
+            var p = NotebookCommunication.getCurrent();
+
+            p.then(function(currID){
+                expect(currID).toBe(currentNotebook.NotebookID);
+            });
+        });
+
+        $httpBackend.flush();
+
+    });
+
+    it("should set a notebook as private and as public", function() {
+
+        var testId = 'simple1ID';
+
+        // mock for get metadata new a notebook is created by new notebook
+        $httpBackend
+            .when('GET', NameSpace.get('asNBMeta', {id: testId}))
+            .respond(testNotebooks.simple1);
+
+        // http mock for login
+        $httpBackend.whenGET(NameSpace.get('asUsersCurrent')).respond(userLoggedIn);
+        $httpBackend.whenPUT(NameSpace.get('asNBPrivate'), {id: testId}).respond();
+        $httpBackend.whenPUT(NameSpace.get('asNBPublic'), {id: testId}).respond();
+
+        MyPundit.login().then(function(){
+            // create notebook and add to notebook exchange
+            new Notebook(testId, true).then(function(){
+
+                // set new notebook as private
+                NotebookCommunication.setPrivate(testId).then(function(){
+
+                    // get notebook from notebook exchange...
+                    var npr = NotebookExchange.getNotebookById(testId);
+
+                    // ...and check if is visibility is set to private
+                    expect(npr.visibility).toBe("private");
+
+                    // set notebook to public
+                    NotebookCommunication.setPublic(npr.id).then(function(){
+
+                        // get notebook from notebook exchange...
+                        var npb = NotebookExchange.getNotebookById(testId);
+
+                        // ...and check if is visibility is set to public
+                        expect(npb.visibility).toBe("public");
+                    });
+                });
+
+            });
+
+        });
+
+        $httpBackend.flush();
+
+    });
+
+    it("should set a notebook as current", function() {
+
+        var testId = 'simple1ID';
+
+        var currentNotebook = {
+            NotebookID: testId
+        };
+
+        // mock for get metadata new a notebook is created by new notebook
+        $httpBackend
+            .when('GET', NameSpace.get('asNBMeta', {id: testId}))
+            .respond(testNotebooks.simple1);
+
+        // http mock for login
+        $httpBackend.whenGET(NameSpace.get('asUsersCurrent')).respond(userLoggedIn);
+        $httpBackend.whenPUT(NameSpace.get('asNBCurrent'+"/"+testId)).respond();
+        $httpBackend.whenGET(NameSpace.get('asNBCurrent')).respond(currentNotebook);
+
+        MyPundit.login().then(function(){
+            new Notebook(testId, true).then(function(){
+
+                // get notebook from notebook exchange...
+                var n = NotebookExchange.getNotebookById(testId);
+
+                // ... and set that notebook as current
+                NotebookCommunication.setCurrent(n.id).then(function(){
+
+                    // check if notebook is set as current by http calling...
+                    NotebookCommunication.getCurrent().then(function(currNbID){
+                        expect(currNbID).toBe(testId);
+                    });
+
+                    // ... and in notebook exchange
+                    var currentNotebook = NotebookExchange.getCurrentNotebooks();
+                    expect(currentNotebook.id).toBe(testId);
+                });
+
+            });
+        });
+
+        $httpBackend.flush();
+
+    });
+
+    it("should edit label of a notebook", function() {
+
+        var testId = 'simple1ID';
+
+        var data = {"NotebookName": 'edited name'};
+
+        // mock for get metadata new a notebook is created by new notebook
+        $httpBackend
+            .when('GET', NameSpace.get('asNBMeta', {id: testId}))
+            .respond(testNotebooks.simple1);
+
+        // http mock for login
+        $httpBackend.whenGET(NameSpace.get('asUsersCurrent')).respond(userLoggedIn);
+        $httpBackend.whenPUT(NameSpace.get('asNBEditMeta',{id: testId}), data).respond();
+
+        // get login
+        MyPundit.login().then(function(){
+            // create a new notebook
+            new Notebook(testId, true).then(function(){
+
+                // get the notebook from notebook exchange and check that his label is not modified yet
+                var n = NotebookExchange.getNotebookById(testId);
+                expect(n.label).not.toBe(data.NotebookName);
+
+                // edit notebook label
+                NotebookCommunication.setName(n.id, data.NotebookName).then(function(){
+
+                    // and check if notebook label is updated
+                    n = NotebookExchange.getNotebookById(testId);
+                    expect(n.label).toBe(data.NotebookName);
+                });
+            });
+
+        });
+
+        $httpBackend.flush();
+
+    });
+
+    it("should delete a notebook", function() {
+
+        var testId = 'simple1ID';
+
+        // mock for get metadata new a notebook is created by new notebook
+        $httpBackend
+            .when('GET', NameSpace.get('asNBMeta', {id: testId}))
+            .respond(testNotebooks.simple1);
+
+        // http mock for login
+        $httpBackend.whenGET(NameSpace.get('asUsersCurrent')).respond(userLoggedIn);
+        $httpBackend.whenDELETE(NameSpace.get('asNB'+"/"+testId)).respond();
+
+        // get login
+        MyPundit.login().then(function(){
+            // create a new notebook
+            new Notebook(testId, true).then(function(){
+
+                // get the notebook from notebook exchange and check that exist
+                var n = NotebookExchange.getNotebookById(testId);
+                expect(n).toBeDefined();
+
+                // delete the notebook
+                NotebookCommunication.deleteNotebook(n.id).then(function(){
+
+                    // and check that notebook doesn't exist anymore!
+                    n = NotebookExchange.getNotebookById(testId);
+                    expect(n).toBeUndefined();
+                });
+            });
+
+        });
+
+        $httpBackend.flush();
 
     });
 
