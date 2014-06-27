91c183a3cf357fdb465edc571ea5f8739cc43059
Giacomo <donatigiacomo91@gmail.com>
2014-06-27 11:52:46 +0200

    [TEST:259] add my items e2e test and minor fix

--- test/e2e/myItems.e2e.test.js
+++ test/e2e/myItems.e2e.test.js
@@ -8,12 +8,14 @@ describe("My Items interaction", function() {
                     loginStatus: 1
                 };
 
+                $httpBackend.whenGET(NameSpace.get('asUsersCurrent')).respond(userLoggedIn);
+
                 var myItems = {
                     value: [
                         {
-                            description: "world",
+                            description: "Text fragment description",
                             isPartOf: "http://fake-url.it/release_bot/build/examples/dante-1.html",
-                            label: "world",
+                            label: "Text fragment label",
                             pageContext: "http://localhost:9000/app/examples/client.html",
                             type: ["http://purl.org/pundit/ont/ao#fragment-text"],
                             uri: "http://fake-url.it/xpointerUriVeryLong"
@@ -21,12 +23,8 @@ describe("My Items interaction", function() {
                     ]
                 };
 
-                var anyItem = {
-                    value: []
-                };
-
-                $httpBackend.whenGET(NameSpace.get('asUsersCurrent')).respond(userLoggedIn);
-                $httpBackend.whenGET(NameSpace.get('asPref', { key:'favorites'})).respond(anyItem);
+                $httpBackend.whenGET(NameSpace.get('asPref', { key:'favorites'})).respond(myItems);
+                $httpBackend.whenPOST(NameSpace.get('asPref', { key:'favorites'})).respond({});
             });
     };
 
@@ -47,6 +45,66 @@ describe("My Items interaction", function() {
             expect(items.length).toBe(1);
             expect(items[0].getText()).toEqual("No my items found.");
         });
+    });
+
+    it('should correctly show my items', function() {
+        p.findElement(protractor.By.css('.pnd-test-get-my-items')).click();
+        // check active tab content
+        p.findElements(protractor.By.css('.pnd-tab-content .active item')).then(function(items) {
+            expect(items.length).toBe(1);
+            expect(items[0].getAttribute('uri')).toEqual('http://fake-url.it/xpointerUriVeryLong');
+        });
+    });
+
+    it('should correctly open confirm modal when try to delete all my items', function() {
+        p.findElement(protractor.By.css('.pnd-test-get-my-items')).click();
+        // check active tab content
+        p.findElements(protractor.By.css('.pnd-tab-content .active item')).then(function(items) {
+            expect(items.length).toBe(1);
+            expect(items[0].getAttribute('uri')).toEqual('http://fake-url.it/xpointerUriVeryLong');
+        });
+
+        // click delete btn (this open a confirm modal)
+        p.findElement(protractor.By.css('.pnd-panel-tab-content-footer .my-items-btn-delete')).click();
+        p.findElements(protractor.By.css('.pnd-confirm-modal-container')).then(function(m) {
+            expect(m.length).toBe(1);
+        });
+
+        // click cancel btn (this close the confirm modal)
+        p.findElement(protractor.By.css('.pnd-confirm-modal-cancel')).click();
+        p.findElements(protractor.By.css('.pnd-confirm-modal-container')).then(function(m) {
+            expect(m.length).toBe(0);
+        });
+        // the numbers of items not change
+        p.findElements(protractor.By.css('.pnd-tab-content .active item')).then(function(items) {
+            expect(items.length).toBe(1);
+            expect(items[0].getAttribute('uri')).toEqual('http://fake-url.it/xpointerUriVeryLong');
+        });
+
+    });
+
+    it('should correctly open confirm modal and delete all my items', function() {
+        p.findElement(protractor.By.css('.pnd-test-login')).click();
+        p.findElement(protractor.By.css('.pnd-test-get-my-items')).click();
+        // check active tab content
+        p.findElements(protractor.By.css('.pnd-tab-content .active item')).then(function(items) {
+            expect(items.length).toBe(1);
+            expect(items[0].getAttribute('uri')).toEqual('http://fake-url.it/xpointerUriVeryLong');
+        });
+
+        // click delete btn (this open a confirm modal)
+        p.findElement(protractor.By.css('.pnd-panel-tab-content-footer .my-items-btn-delete')).click();
+        p.findElements(protractor.By.css('.pnd-confirm-modal-container')).then(function(m) {
+            expect(m.length).toBe(1);
+        });
+
+        // click confirm btn (this close the confirm modal)
+        p.findElement(protractor.By.css('.pnd-confirm-modal-confirm')).click();
+        // the number of items now is zero
+        p.findElements(protractor.By.css('.pnd-tab-content .active item')).then(function(items) {
+            expect(items.length).toBe(0);
+        });
+
     });
 
 });
