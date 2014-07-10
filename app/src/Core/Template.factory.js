d51eaed3f2bc13ca050b20311de044a8904049d0
Giacomo <donatigiacomo91@gmail.com>
2014-07-10 10:16:23 +0200

    add template factory

---
+++ app/src/Core/Template.factory.js
@@ -0,0 +1,29 @@
+angular.module('Pundit2.Core')
+.factory('Template', function(BaseComponent, Utils, TemplatesExchange) {
+
+    var templateComponent = new BaseComponent('Template');
+
+    var TemplateFactory = function(id, values) {
+        // To create a new Template at least a id is needed
+        if (typeof(id) === "undefined") {
+            templateComponent.err("Can't create a template without an id");
+            return;
+        }
+        this.id = id;
+
+        if (angular.isObject(values)) {
+            templateComponent.log('Extending new Template with values '+this.id, values);
+            Utils.deepExtend(this, values);
+        }
+
+        // Add it to the exchange, ready to be .. whatever will be.
+        TemplatesExchange.addTemplate(this);
+    };
+
+    TemplateFactory.prototype.fun = function() {
+        
+    };
+
+    return TemplateFactory;
+
+});
--- app/src/Core/TemplatesSelector.service.js
+++ app/src/Core/TemplatesSelector.service.js
@@ -1,6 +1,6 @@
 angular.module('Pundit2.Core')
 .service('TemplatesSelector', function(BaseComponent, Config, $http, $q,
-    Item, ItemsExchange, TemplatesExchange, FreebaseSelector, SelectorsManager) {
+    Item, Template, ItemsExchange, TemplatesExchange, FreebaseSelector, SelectorsManager) {
     
     var templatesSelector = new BaseComponent("TemplatesSelector");
 
@@ -56,40 +56,48 @@ angular.module('Pundit2.Core')
                     data.color = color;                    
                 }
 
-                // chek if need to get items from freebase
-                var trp = data.triples;
-                for(var i in trp) {
+                var i,
+                    trp = data.triples;
+
+                for(i in trp) {
                     // read predicate and made a real item
                     if (typeof(trp[i].predicate) !== 'undefined') {
                         trp[i].predicate.type = ["http://www.w3.org/1999/02/22-rdf-syntax-ns#Property"];
                         trp[i].predicate = new Item(trp[i].predicate.uri, trp[i].predicate);
                         ItemsExchange.addItemToContainer(trp[i].predicate, Config.modules.Client.relationsContainer);
                     }
+                }
+                
+                var tmpl = new Template(data.id, data);
+                for(i in trp) {
                     // if the type is an uri we must get items from uri
                     if (typeof(trp[i].object) !== 'undefined' && trp[i].object.type === 'uri') {
                         // identify how selector use by parsing the uri
                         // now we support only freebase
                         if (trp[i].object.value.indexOf('freebase') > -1) {
-                            var mid = trp[i].object.value.substring(23);
-                            // make an incomplete item
-                            // which will be completed by the selector
-                            var item = {
-                                mid: mid,
-                                image: 'https://usercontent.googleapis.com/freebase/v1/image' + mid,
-                                description: -1,
-                                uri: -1
-                            };
-                            var p = $q.defer();
-                            p.promise.then(function(ret){
-                                // when promise is resolved the item
-                                // is compleated, override uri with complete item values
-                                trp[i].object.type = 'item';
-                                trp[i].object.value = ret;
-                                // TODO how label use?
-                                trp[i].object.value.label = trp[i].object.value.description;
-                                trp[i].object.value = new Item(trp[i].object.value.uri, trp[i].object.value);
-                            });
-                            selector.getItemDetails(item, p);
+                            (function closure(_i) {
+                                var mid = trp[i].object.value.substring(23);
+                                // make an incomplete item
+                                // which will be completed by the selector
+                                var item = {
+                                    mid: mid,
+                                    image: 'https://usercontent.googleapis.com/freebase/v1/image' + mid,
+                                    description: -1,
+                                    uri: -1
+                                };
+                                var p = $q.defer();
+                                p.promise.then(function(ret){
+                                    // when promise is resolved the item
+                                    // is compleated, override uri with complete item values
+                                    tmpl.triples[_i].object.type = 'item';
+                                    tmpl.triples[_i].object.value = ret;
+                                    // TODO how label use?
+                                    tmpl.triples[_i].object.value.label = tmpl.triples[_i].object.value.description;
+                                    tmpl.triples[_i].object.value = new Item(tmpl.triples[_i].object.value.uri, tmpl.triples[_i].object.value);
+                                });
+                                selector.getItemDetails(item, p);
+                            })(i);
+                            
                         }
                     }
                 }
@@ -97,8 +105,8 @@ angular.module('Pundit2.Core')
                 // TODO at this point we have the template info
                 // but is possible that we not have the items info
                 // if we have get it eg. from freebase
-                TemplatesExchange.addTemplate(data);
-                promise.resolve(data);
+                console.log(tmpl);
+                promise.resolve(tmpl);
 
             })
             .error(function(){
