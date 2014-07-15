3af6e048ec5e2f1ff7ae3e8e3a1daecaa3d12cad
SimoneSabba <sabbatini.simone@gmail.com>
2014-07-15 11:46:24 +0200

    added handler languages in korboee

--- app/src/KorboEE/New/KorboEE.new.ctrl.js
+++ app/src/KorboEE/New/KorboEE.new.ctrl.js
@@ -3,50 +3,68 @@ angular.module('KorboEE')
 
         $scope.tabs = [];
         $scope.imageUrl = "";
-        $scope.tabs.push({
-            title: 'EN',
-            label: '',
-            description: '',
-            content: 'English'
-        });
 
-        $scope.tabs.push({
-            title: 'EN',
-            label: '',
-            description: '',
-            content: 'Italian'
-        });
+        var tooltipMessageTitle = "Insert title of the entity in ";
+        var tooltipMessageDescription = "Insert description of the entity in ";
+        var errorMandatory = "The Title field is mandatory and must be filled";
+        var errorLabelTooShort = " The Title must be contain at least " + $scope.conf.labelMinLength +" characters";
 
-        $scope.tabs.push({
-            title: 'EN',
-            label: '',
-            description: '',
-            content: 'Spanish'
-        });
 
-        $scope.tabs.push({
-            title: 'EN',
-            label: '',
-            description: '',
-            content: 'English'
-        });
+        //build languages tabs
+        for(var i=0; i< $scope.conf.languages.length; i++){
 
-        $scope.tabs.push({
-            title: 'EN',
-            label: '',
-            description: '',
-            content: 'English'
-        });
+            if($scope.conf.languages[i].state){
+                var title = angular.uppercase($scope.conf.languages[i].value);
+                var name = angular.lowercase($scope.conf.languages[i].name);
+                var lang = {
+                    'title': title,
+                    'name' : $scope.conf.languages[i].name,
+                    'description': "",
+                    'label': "",
+                    'mandatory': true,
+                    'hasError': false,
+                    'tooltipMessageTitle': tooltipMessageTitle + name,
+                    'tooltipMessageDescription': tooltipMessageDescription + name,
+                    'tooltipMessageError': "message"
+                };
 
-        $scope.tabs.push({
-            title: 'EN',
-            label: '',
-            description: '',
-            content: 'English'
-        });
+                $scope.tabs.push(lang);
+            }
+        }
+
+    // check if language field are all right filled
+    var checkLanguages = function(){
+        var allLangAreOk = true;
+        for(var l=0; l<$scope.tabs.length; l++){
+
+            (function(index) {
+                if(typeof($scope.tabs[index].label) === 'undefined' || $scope.tabs[index].label === ''){
+                    $scope.tabs[index].hasError = true;
+                    allLangAreOk = false;
+                    $scope.tabs[index].tooltipMessageError = errorMandatory;
+                } else if($scope.tabs[index].label.length < $scope.conf.labelMinLength){
+                    $scope.tabs[index].hasError = true;
+                    allLangAreOk = false;
+                    $scope.tabs[index].tooltipMessageError = errorLabelTooShort;
+                } else
+                {
+                    $scope.tabs[index].hasError = false;
+                }
+
+            })(l);
+
+        }
+
+        return allLangAreOk;
+    };
 
     $scope.save = function(){
-        console.log($scope.imageUrl);
+        /*for(var i=0; $scope.tabs.length; i++){
+            console.log($scope.tabs[i]);
+        }*/
+        var checkLang = checkLanguages();
+        console.log("saved ", checkLang);
+        checkLanguages();
     };
 
 
