48c851107446e7c71cee76084e3a9ecfde27a81a
SimoneSabba <sabbatini.simone@gmail.com>
2014-07-18 11:50:25 +0200

    added loading and error status in preview directive

--- app/src/Preview/Preview.dir.js
+++ app/src/Preview/Preview.dir.js
@@ -3,7 +3,9 @@ angular.module('Pundit2.Preview')
         return {
             restrict: 'E',
             scope: {
-                useInKorbo: '@'
+                useInKorbo: '@',
+                isLoading: '@',
+                errorLoading: '@'
             },
             templateUrl: "src/Preview/DashboardPreview.dir.tmpl.html",
             controller: 'PreviewCtrl'
