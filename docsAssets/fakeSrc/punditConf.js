4005911e3685e23a984d9d0c9dc9ce03d6ae2376
Giacomo <donatigiacomo91@gmail.com>
2014-07-22 13:08:26 +0200

    fix doc

--- docsAssets/fakeSrc/punditConf.js
+++ docsAssets/fakeSrc/punditConf.js
@@ -62,7 +62,6 @@ angular.module('punditConfig');
  *  - {@link #!/api/punditConfig/object/modules#TextFragmentHandler TextFragmentHandler}
  *  - {@link #!/api/punditConfig/object/modules#Toolbar Toolbar}
  *  - {@link #!/api/punditConfig/object/modules#TripleComposer TripleComposer}
- *  - {@link #!/api/punditConfig/object/modules#VocabulariesContainer VocabulariesContainer}
  *
  * All the configurable properties of the modules are available within {@link #!/api/punditConfig/object/modules modules}. 
  **/
