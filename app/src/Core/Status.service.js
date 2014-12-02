34a6eea87a340283f7638490ce8cf3a887334416
Raffaele Masotti <raffaele.masotti@gmail.com>
2014-12-02 13:03:56 +0100

    Status service partial ref

--- app/src/Core/Status.service.js
+++ app/src/Core/Status.service.js
@@ -1,20 +1,22 @@
 angular.module('Pundit2.Core')
+
 .constant("STATUSDEFAULTS", {
     debug: false
 })
-.service('Status', function (BaseComponent, EventDispatcher, STATUSDEFAULTS) {
+
+.service('Status', function(BaseComponent, EventDispatcher, STATUSDEFAULTS) {
 
     var status = new BaseComponent('Status');
 
     var state = {
-            AnnotationSidebar: {},
-            Dashboard: {},
-            Toolbar: {},
-            Pundit: {
-                clientBoot: false,
-                userLogged: false,
-                loading: false
-            }
+        AnnotationSidebar: {},
+        Dashboard: {},
+        Toolbar: {},
+        Pundit: {
+            clientBoot: false,
+            userLogged: false,
+            loading: false
+        }
     };
 
     var errorLog = [],
@@ -24,7 +26,7 @@ angular.module('Pundit2.Core')
         EventDispatcher.sendEvent('Pundit.loading', state.Pundit.loading);
     };
 
-    var setLoading = function(currentState) {
+    var setLoading = function(currentState)  {
         if (currentState) {
             loadingCount++;
             state.Pundit.loading = true;
@@ -40,49 +42,57 @@ angular.module('Pundit2.Core')
     }
 
     // Loading
-    EventDispatcher.addListeners(['NotebookComposerCtrl.loading', 'MyItems.loading', 
-        'NotebookCommunication.loading', 'AnnotationsCommunication.loading', 'Annomatic.loading'], 
-        function (e) {
+    EventDispatcher.addListeners(
+        [
+            'AnnotationsCommunication.loading',
+            'NotebookCommunication.loading',
+            'NotebookComposerCtrl.loading',
+            'Annomatic.loading',
+            'MyItems.loading'
+
+        ],
+        function(e) {
             setLoading(e.args);
-    });    
+        }
+    );
 
     // Client
-    EventDispatcher.addListener('Client.boot', function () {
+    EventDispatcher.addListener('Client.boot', function() {
         state.Pundit.clientBoot = true;
     });
 
     // MyPundit
-    EventDispatcher.addListener('MyPundit.isUserLogged', function (e) {
+    EventDispatcher.addListener('MyPundit.isUserLogged', function(e) {
         state.Pundit.userLogged = e.args;
     });
 
     // AnnotationSidebar
-    EventDispatcher.addListener('AnnotationSidebar.toggle', function (e) {
+    EventDispatcher.addListener('AnnotationSidebar.toggle', function(e) {
         state.AnnotationSidebar.isExpanded = e.args;
     });
 
     // Error
-    EventDispatcher.addListener('Pundit.error', function (e) {
+    EventDispatcher.addListener('Pundit.error', function(e) {
         errorLog.push(e.args);
     });
 
-    status.getState = function (component) {
+    status.getState = function(component) {
         return state[component];
     };
 
-    status.getClientBoot = function () {
+    status.getClientBoot = function() {
         return state.Pundit.clientBoot;
     };
 
-    status.getLoading = function () {
+    status.getLoading = function() {
         return state.Pundit.loading;
-    };    
+    };
 
-    status.getUserStatus = function () {
+    status.getUserStatus = function() {
         return state.Pundit.userLogged;
     };
 
-    status.getLog = function () {
+    status.getLog = function() {
         return errorLog;
     };
 
