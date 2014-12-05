29a861080c4d0a4a7f84a8f26417e5f753fcc862
Raffaele Masotti <raffaele.masotti@gmail.com>
2014-12-05 12:52:03 +0100

    Notebook and item conformity

--- app/src/Notebook/Notebook.dir.js
+++ app/src/Notebook/Notebook.dir.js
@@ -1,9 +1,13 @@
+// TODO: remove this directive and use Item
+
 angular.module('Pundit2.Notebook')
 .directive('notebook', function() {
     return {
         restrict: 'E',
         scope: {
             nid: '@',
+            hideStickyButton: '@',
+            forceSticky: '=forceSticky',
             menuType: '@'
         },
         templateUrl: "src/Notebook/Notebook.dir.tmpl.html",
